5880614d07da658f89bbc8e540df3eff5b17c7f9
marina <nicolaimarina@gmail.com>
2023-01-24 14:07:26 -0500

    adding the table head in the input area

--- apps/expense/components/InputArea/InputArea.tsx
+++ apps/expense/components/InputArea/InputArea.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import { Container, Box ,Button} from '@shared/core';
 import { TableItem } from '@components/TableItem/TableItem';
+import { listenerCount } from 'process';
 
-const InputArea = () => {
-  return (
-    <Container> 
-      <Box
-       display="grid"
-        p={{ _: 8, xl: 4 }}
-        width={{ _: 1, lg: 0.8, xl: 0.6 }}
-        mx={{ _: 0, lg: "auto" }}
-       
-        gridAutoFlow={{ _: "row", lg: "column" }}
-      >
-       <TableItem size = {18} item = {`Date`} fontWeight={'normal'} margin={'auto'}/>
-       <TableItem size = {18} item = {`Category`} fontWeight={'normal'} margin={'auto'} />
-       <TableItem size = {18} item = {`Description`} fontWeight={'normal'} margin={'auto'} />
-       <TableItem size = {18} item = {`Amount`} fontWeight={'normal'} margin={'auto'} />
-       <Button variant="tonal">Add</Button>
-       </Box>
-    </Container>
+type Props = {
+  list: []
+}
+
+const InputArea = ({list} :Props) => {
+  return (   
+     <>
+        <table>
+          <thead  >
+            <tr>
+             <th><TableItem size = {18} item = {`Date`} fontWeight={'normal'} margin={'auto'}/></th>
+             <th><TableItem size = {18} item = {`Category`} fontWeight={'normal'} margin={'auto'} /></th>
+             <th><TableItem size = {18} item = {`Description`} fontWeight={'normal'} margin={'auto'} /></th>
+             <th><TableItem size = {18} item = {`Amount`} fontWeight={'normal'} margin={'auto'} /></th>
+              <th> <Button variant="tonal">Add</Button></th>
+           </tr>
+         </thead>
+       </table>
+       <tbody>
+        {/* {list?.map((item,index) => (
+        <TableItem size = {18} item={item} fontWeight={'normal'} margin={'auto'} key={index}/>))} */}
+       </tbody>
+    </>   
   )
 }
 
