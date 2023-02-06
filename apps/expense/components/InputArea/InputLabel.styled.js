f6ebf2d0c573a6616ecf31c9a10cfc4a7c5ba289
marina <nicolaimarina@gmail.com>
2023-02-06 18:26:52 -0500

    styling the label

---
+++ apps/expense/components/InputArea/InputLabel.styled.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "@emotion/styled";
+
+
+const InputLabel = styled.label`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
+`;
+
+
+export default InputLabel;
